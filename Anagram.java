53deeb047a16c4dc7b70eaf23b4c83c8a82b1e8d
Dhruv Sethi <dsethi@paypalcorp.com>
2016-08-04 12:16:24 +0530

    Cleaning code

--- Anagram.java
+++ Anagram.java
@@ -14,13 +14,9 @@ public class Anagram {
 			Scanner sc = readWords();
 			Map<String, List<String>> wordAnagramPairs = new HashMap<>();
 			wordAnagramPairs = mapAnagrams(sc);
-			for (String anagram : wordAnagramPairs.keySet()){
-				if (wordAnagramPairs.get(anagram).size() > 1){
+			for (String anagram : wordAnagramPairs.keySet())
+				if (wordAnagramPairs.get(anagram).size() > 1)
 					System.out.println(wordAnagramPairs.get(anagram));
-				}
-				
-			}
-			
 		} catch (FileNotFoundException e) {
 			System.out.println("File Not Found");
 		}
@@ -28,24 +24,22 @@ public class Anagram {
 
 	private static Map<String, List<String>> mapAnagrams(Scanner sc) {
 		String currWord, currSortedWord;
-		Map<String, List<String>> result = new HashMap<>();
-		//List<String> wordList = new ArrayList<>();
+		Map<String, List<String>> anagramList = new HashMap<>();
 		while (sc.hasNext()) {
 			currWord = sc.next();
 			currSortedWord = sortedWord(currWord);
-			if(null == result.get(currSortedWord)){
-
-				result.put(currSortedWord, new ArrayList<String>());
-				result.get(currSortedWord).add(currWord);
-			}
-			else{
-				result.get(currSortedWord).add(currWord);
-//				wordList.addAll(result.get(currSortedWord));
-//				wordList.add(currWord);
-			}
-//			result.put(currSortedWord, wordList);
-//			wordList.clear();
+			anagramList = addAnagram(anagramList, currWord, currSosrtedWord);
 		}
+		return anagramList;
+	}
+
+	private static Map<String, List<String>> addAnagram(Map<String, List<String>> result, String currWord,
+			String currSortedWord) {
+		if (null == result.get(currSortedWord)) {
+			result.put(currSortedWord, new ArrayList<String>());
+			result.get(currSortedWord).add(currWord);
+		} else
+			result.get(currSortedWord).add(currWord);
 		return result;
 	}
 
@@ -57,7 +51,7 @@ public class Anagram {
 	}
 
 	private static Scanner readWords() throws FileNotFoundException {
-		String path = "C:\\Users\\hsivasubramanian\\workspace\\anagram\\sowpods.txt";
+		String path = "C:\\Users\\dsethi\\Desktop\\sowpods.txt";
 		Scanner sc = new Scanner(new File(path));
 		return sc;
 	}
